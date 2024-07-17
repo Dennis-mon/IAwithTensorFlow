70a27f577242074ea03d99b82fbc24c152d9f966
Dennis-mon <dennismonge221@gmail.com>
2024-07-17 13:03:21 +0200

    Regresión Lineal y LoadCSV integrados

--- Regresion_Lineal/src/index.js
+++ Regresion_Lineal/src/index.js
@@ -1,9 +1,12 @@
 require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
 const loadCSV = require('./loadCSV');
+const RegresionLineal = require('./regresion_lineal'); // Cargamos la clase encargada de realizar la regresión lineal
 
-const {features, labels, testFeatures, testLabels} = loadCSV(
-    './src/csv/datos.csv',
+const pathCSV = './src/csv/';
+
+/*const {features, labels, testFeatures, testLabels} = loadCSV(
+    `${pathCSV}datos.csv`,
     ',', 
     {
         dataColumns: ['mes','dia'],
@@ -17,4 +20,27 @@ const {features, labels, testFeatures, testLabels} = loadCSV(
 console.log("features: ", features );
 console.log("labels: ", labels );
 console.log("testFeatures: ", testFeatures );
+console.log("testLabels: ", testLabels );*/
+
+
+const { features , labels, testFeatures, testLabels } = loadCSV(`${pathCSV}cars.csv`, ',', {
+    shuffle: true,
+    splitTest: 50,
+    dataColumns: ['horsepower'],
+    labelColumns: ['mpg']
+})
+
+const regresionPrueba = new RegresionLineal(features, labels, {
+    learningRate: 0.00001,
+    iterations: 100
+});
+
+console.log("features: ", features );
+console.log("labels: ", labels );
+console.log("testFeatures: ", testFeatures );
 console.log("testLabels: ", testLabels );
+
+regresionPrueba.train();
+
+console.log('Update M => ', regresionPrueba.m);
+console.log('Update B => ', regresionPrueba.b);
