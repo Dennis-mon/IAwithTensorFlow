efdcf7762de553d2fdc4657fa25bcf7ea4304065
Dennis-mon <dennismonge221@gmail.com>
2024-07-22 10:29:31 +0200

    Herramienta para plots

--- Regresion_Lineal/src/index.js
+++ Regresion_Lineal/src/index.js
@@ -2,18 +2,19 @@ require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
 const loadCSV = require('./loadCSV');
 const RegresionLineal = require('./regresion_lineal'); // Cargamos la clase encargada de realizar la regresión lineal
+const plot = require('node-remote-plot'); // Pacakage para generar gráficos
 
 const pathCSV = './src/csv/';
 
 //cargamos csv 
 const { features , labels, testFeatures, testLabels } = loadCSV(
-    `${pathCSV}datos-marzo-horas.csv`,
+    `${pathCSV}cars.csv`,
     ',',
     {
         shuffle: true,
         splitTest: 9,
-        dataColumns: ['dia', 'mes', '0-24'],
-        labelColumns: ['value']
+        dataColumns: ['horsepower', 'weight', 'displacement'],
+        labelColumns: ['mpg']
     }
 )
 
@@ -22,7 +23,7 @@ const regresionPrueba = new RegresionLineal(
     features, 
     labels,
     {
-        learningRate: 10,
+        learningRate: 0.1,
         iterations: 100
     }
 );
@@ -46,3 +47,12 @@ console.log('Datos =>' , features[1]);
 console.log('Resultado Real =>' , labels[1]);
 const resultado = regresionPrueba.predictResult(features[1]);
 console.log('Resultado Predicción =>' ,resultado);
+
+//Gráfico del resultado
+console.log('==========================');
+
+plot({   
+    x: regresionPrueba.mseHistory.reverse(),
+    xLabel: 'Iteration #',
+    yLabel: 'Mean Square Error'
+});
