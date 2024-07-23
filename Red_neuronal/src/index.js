cf6eb95d5f126d76c71fa5112a28a459c380fcd8
Dennis-mon <dennismonge221@gmail.com>
2024-07-23 14:32:09 +0200

    Comentar el código

--- Red_neuronal/src/index.js
+++ Red_neuronal/src/index.js
@@ -4,14 +4,14 @@ const loadCSV = require('./loadCSV.js');
 const RedNeuronal = require('./red_neuronal.js'); // Cargamos la clase encargada de realizar la regresión lineal
 const pathCSV = '../csv/';
 
-//cargamos csv 
+//Cargamos CSV 
 const { features , labels, testFeatures, testLabels } = loadCSV(
     `${pathCSV}datos-marzo-horas.csv`,
     ',',
     {
         shuffle: true,
         splitTest: 9,
-        dataColumns: ['dia'],
+        dataColumns: ['dia', 'mes', '0-24'],
         labelColumns: ['value']
     }
 )
@@ -41,9 +41,16 @@ console.log(models);
 
 */
 
+
+//Creamos una nueva instancia de la clase Res Neuronal
 const redneuronalPrueba = new RedNeuronal(
-    features, 
-    labels,
+    features, //Variables Independientes
+    labels,   //Variables Dependientes
+    //Configuración
+    //learningRate = tasa de aprendizaje // por defecto 0.1
+    //epochs       = nº de veces que se entrena el algoritmo para todos los datos // por fecefto 2000 
+    //neurons      = nº de neuronas que queramos que tenga el algoritmo // por defecto 10
+    //activation   = función de activación que vamos a usar para el algoritmo, se recomienda ('relu' , 'tanh', 'sigmoid', 'lineal') // por defecto 'relu'
     {
         learningRate: 0.1,
         epochs: 2000,
@@ -51,17 +58,18 @@ const redneuronalPrueba = new RedNeuronal(
     }
 );
 
+//Función asincrona que se encarga de compilar y entrenar el algorimto ademas de generar una prediccón para unos valores independientes de entrada(features)
 async function crearRedes(){
 
     redneuronalPrueba.compilar();
-    await redneuronalPrueba.entrenar();
+    await redneuronalPrueba.entrenar(); //await para impedir que el código se siga ejecutando hasta que el algoritmo este 100% entrenado
+
+    //Guardamos los resultados de la predicción en una variable auxilar para poder verlos en la terminal
     const resultado = redneuronalPrueba.prediccion(features);
-    
     console.log("Resultados obtenidos:");
     resultado.print();
     console.log("Resultados reales:");
     console.log(labels);
 }
-
 
 crearRedes();
--- Red_neuronal/src/red_neuronal.js
+++ Red_neuronal/src/red_neuronal.js
@@ -1,20 +1,26 @@
 const tf = require('@tensorflow/tfjs'); // TensorFlow
 
-
+//Calse encargada de toda la lógia de un algoritmo hecho por redes neuronales
 class RedNeuronal{
 
+    //Features = Datos , Labels = Resultados, Options = Opciones del algoritmo 
     constructor(features, labels, options){
+
+        //Declaramos los valores independientes (features) y los valores dependientes (labels)
         this.features = tf.tensor(features);
         this.labels   = tf.tensor(labels);
 
+        //Declaramos el modelo del algoritmo
         this.model = tf.sequential();
 
+        //Declaramos las opciones
         this.options = Object.assign({ epochs: 2000, learningRate: 0.1, neurons: 10, activation: 'relu' }, options);
-        console.log(this.options.epochs);
     };
 
+    //Método que se encarga de compilar el algoritmo de red neuronal
     compilar(){
 
+        //Se crean las capas que componen la red neuronal y se añaden al modelo
         const hiddenLayer = tf.layers.dense({
             units: this.options.neurons,
             inputShape: [ this.features.shape[1] ],
@@ -30,6 +36,7 @@ class RedNeuronal{
     
         this.model.add(outputLayer);
     
+        //Compilamos nuestro modelo con los parámetros de optimización y de pérdida de error
         this.model.compile({
             optimizer: tf.train.adam(this.options.learningRate),
             loss: 'meanSquaredError'
@@ -37,15 +44,16 @@ class RedNeuronal{
     
     };
 
+    //Método que se encarga de entrenar el algoritmo con los features y labels que se le pasan en el contructor
     async entrenar(){
         console.log('Entrenando');
-        console.log(this.options.epochs);
         await this.model.fit(this.features, this.labels, { epochs: this.options.epochs });
     };
 
-    prediccion(inputs){
-        inputs = tf.tensor(inputs)
-        return this.model.predict( inputs );
+    //Método que predice y devuelvo unos resultados a partir de los datos de entrada que se le propocionen(features)
+    prediccion(features){
+        features = tf.tensor(features)
+        return this.model.predict( features );
     }
 }
 
