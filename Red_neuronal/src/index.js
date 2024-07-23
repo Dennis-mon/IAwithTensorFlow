07e85257d3075acc9d6415f6a77c433dfa96b4d4
AbelMartinezFlores <abelmartinezlamola@gmail.com>
2024-07-23 12:44:04 +0200

    avances en el algoritmo

--- Red_neuronal/src/index.js
+++ Red_neuronal/src/index.js
@@ -2,12 +2,33 @@ require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
 const loadCSV = require('./loadCSV.js');
 
+const pathCSV = '../csv/';
 
-let xs = [ [0,0],[0,1],[1,0],[1,1] ];
-let ys = [ [0], [1], [1], [1] ];
+//cargamos csv 
+const { features , labels, testFeatures, testLabels } = loadCSV(
+    `${pathCSV}datos-marzo-horas.csv`,
+    ',',
+    {
+        shuffle: true,
+        splitTest: 9,
+        dataColumns: ['dia'],
+        labelColumns: ['value']
+    }
+)
 
-let inputs = tf.tensor2d(xs);
-let outputs = tf.tensor2d(ys);
+//let xs = [ [0,0],[0,1],[1,0],[1,1] ];
+//let ys = [ [0], [1], [1], [1] ];
+
+
+
+let inputs = tf.tensor(features);
+let outputs = tf.tensor(labels);
+
+console.log("inputs:")
+inputs.print()
+console.log("outputs")
+outputs.print()
+
 
 console.log( inputs.shape[0] )
 
@@ -16,7 +37,7 @@ async function createModel(){
 
     const hiddenLayer = tf.layers.dense({
         units: 10,
-        inputShape: [ 2 ],
+        inputShape: [ inputs.shape[1] ],
         activation: 'tanh'
     });
 
@@ -25,31 +46,31 @@ async function createModel(){
     const outputLayer = tf.layers.dense({
         units: 1,
         inputShape: [10],
-        activation: 'tanh'
     });
 
     model.add(outputLayer);
 
     model.compile({
-        optimizer: 'sgd',
+        optimizer: tf.train.adam(0.5),
         loss: 'meanSquaredError'
     });
 
     const configTrain = {
-        epochs: 1000
+        epochs: 5000,
     }
 
     const h = await model.fit(inputs, outputs, configTrain);
 
     console.log(h.history.loss[ h.history.loss.length - 1 ]);
 
-    let prediccion = model.predict( tf.tensor2d(xs) );
+    let prediccion = model.predict( inputs );
 
 
     console.log("Resultados obtenidos:");
     prediccion.print();
     console.log("Resultados reales:");
     outputs.print();
+
 
 
 }
