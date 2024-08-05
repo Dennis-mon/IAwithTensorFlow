3533ff1f012b198cd4dd011f3bab77f0325222f0
Dennis-mon <dennismonge221@gmail.com>
2024-08-05 12:17:38 +0200

    Ejemplo de una anomalía visto gráficamente

--- Isolation Forest/src/index.js
+++ Isolation Forest/src/index.js
@@ -1,5 +1,13 @@
 const IsolationForest = require('ml-isolation-forest');
-const xl = require('excel4node');
+
+const { createCanvas } = require('canvas');
+const {Chart, registerables} = require('chart.js');
+const fs = require('fs');
+
+// Registrar todos los componentes necesarios
+Chart.register(...registerables);
+
+//const xl = require('excel4node');
 
 let X = [
   [200, 50],
@@ -28,6 +36,55 @@ let result = anomalyDetector.predict([
 
 console.log(result);
 
+var anomalias = [];
+var normales  = []
+
+//filtramos los anomalyScore mayores a 0.5
+result.forEach( (element, index) =>{
+  if(element > 0.5) anomalias.push( X[index] );
+  else normales.push( X[index] );
+})
+
+
+/////////////////////////
+
+// Configuración del lienzo (canvas)
+const canvas = createCanvas(800, 600);
+const ctx = canvas.getContext('2d');
+
+const chart = new Chart(ctx, {
+    type: 'scatter',
+    data: {
+        datasets: [
+            {
+                label: 'Datos normales',
+                data: normales,
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            },
+            {
+              label: 'Anomalias',
+              data: anomalias,
+              backgroundColor: 'rgba(153, 102, 255, 0.2)',
+              borderColor: 'rgba(153, 102, 255, 1)',
+              borderWidth: 1
+          },   
+        ]
+    }
+});
+
+// Guarda el gráfico como una imagen PNG
+const buffer = canvas.toBuffer('image/png');
+fs.writeFileSync('chart.png', buffer);
+console.log('Gráfico guardado en chart.png');
+
+//////////////////////////
+
+
+
+/////////////////////////
+/*
 //Creamos el Excel
 var wb = new xl.Workbook();
 
@@ -49,3 +106,5 @@ const pathExcel = path.join(__dirname, 'excel', nombreArchivo + '.xlsx');
 wb.write(pathExcel, function(err, stats){
   if(err) console.log(err);
 });
+*/
+/////////////////////////
